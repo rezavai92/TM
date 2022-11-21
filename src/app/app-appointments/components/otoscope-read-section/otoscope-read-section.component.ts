3a749ae90a91c40db9fffae0bae57734d7f59946
rezavai92 <nijhumreza52@gmail.com>
2022-11-21 09:46:16 +0600

    otosope read section init

---
+++ src/app/app-appointments/components/otoscope-read-section/otoscope-read-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-otoscope-read-section',
+  templateUrl: './otoscope-read-section.component.html',
+  styleUrls: ['./otoscope-read-section.component.scss']
+})
+export class OtoscopeReadSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
