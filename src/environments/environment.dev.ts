4eb5d2d6dcb5c04b9fa3838784de9c4a140571fb
rezavai92 <nijhumreza52@gmail.com>
2022-12-02 11:23:27 +0600

    new domain

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,8 +1,7 @@
-const baseUrl = "https://137.184.124.68";
+const baseUrl = 'https://shifabd.org';
 export const environment = {
-    production: true,
-    baseUrl: `${baseUrl}`,
-    UserService: `${baseUrl}/api/UserService/`,
-    SmsService : `${baseUrl}/api/Sms/`
+	production: true,
+	baseUrl: `${baseUrl}`,
+	UserService: `${baseUrl}/api/UserService/`,
+	SmsService: `${baseUrl}/api/Sms/`,
 };
-
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
-const baseUrl = "https://137.184.124.68";
+const baseUrl = 'https://shifabd.org';
 export const environment = {
-  production: true,
-  baseUrl: `${baseUrl}`,
-  UserService: `${baseUrl}/api/UserService/`,
-  StorageService: `${baseUrl}/api/StorageService/`,
-  SmsService: `${baseUrl}/api/Sms/`,
-  AppCatalogue: `${baseUrl}/api/AppCatalogue/`,
-  Appointment: `${baseUrl}/api/Appointment/`,
+	production: true,
+	baseUrl: `${baseUrl}`,
+	UserService: `${baseUrl}/api/UserService/`,
+	StorageService: `${baseUrl}/api/StorageService/`,
+	SmsService: `${baseUrl}/api/Sms/`,
+	AppCatalogue: `${baseUrl}/api/AppCatalogue/`,
+	Appointment: `${baseUrl}/api/Appointment/`,
 };
