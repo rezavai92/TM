6b532561a350fad8f39db01f210fc04cba7e03fa
rezavai92 <nijhumreza52@gmail.com>
2023-01-20 12:24:26 +0600

    password regex changed

--- src/app/shared/shared-data/constants.ts
+++ src/app/shared/shared-data/constants.ts
@@ -40,9 +40,9 @@ export const numberRegexString = '^[0-9]*$';
 */
 
 
-//export const passwordRegexString = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"; // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+export const passwordRegexString = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"; // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
 
-export const passwordRegexString = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$";
+
 export const MfsList = [
 	{
 		Key: 'BKASH',
